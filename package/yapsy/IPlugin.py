bb2aa4afffe102d536ef747a198516e790e50f35
Ben Hoff <hoff.benjamin.k@gmail.com>
2015-09-11 04:29:15 -0500

    whitespace changes

--- package/yapsy/IPlugin.py
+++ package/yapsy/IPlugin.py
@@ -35,22 +35,22 @@ API
 
 
 class IPlugin(object):
-	"""
-	The most simple interface to be inherited when creating a plugin.
-	"""
+    """
+    The most simple interface to be inherited when creating a plugin.
+    """
 
-	def __init__(self):
-		self.is_activated = False
+    def __init__(self):
+        self.is_activated = False
 
-	def activate(self):
-		"""
-		Called at plugin activation.
-		"""
-		self.is_activated = True
+    def activate(self):
+        """
+        Called at plugin activation.
+        """
+        self.is_activated = True
 
-	def deactivate(self):
-		"""
-		Called when the plugin is disabled.
-		"""
-		self.is_activated = False
+    def deactivate(self):
+        """
+        Called when the plugin is disabled.
+        """
+        self.is_activated = False
 
