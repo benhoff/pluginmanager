2e68871a4e2ad2699f6be33ec64ef0506eeaba5e
Ben Hoff <hoff.benjamin.k@gmail.com>
2015-09-23 22:10:00 -0500

    added class to PluginLoader which controls blacklist state

--- simpleyapsy/plugin_loader.py
+++ simpleyapsy/plugin_loader.py
@@ -1,6 +1,7 @@
 import os
 import sys
-import imp
+import inspect
+from importlib.machinery import SourceFileLoader
 
 def _get_unique_module_name(plugin_info):
     plugin_module_name = 'yapsy_loaded_plugin_{name}'.format(plugin_info['name'])
@@ -12,20 +13,44 @@ def _get_unique_module_name(plugin_info):
 
     return plugin_module_name
 
-def load_plugin(self, plugin_filepaths, plugin_infos):
-    """
-    returns a list of loaded plugins
-    """
-    loaded_plugins = []
-    for plugin_filepath in plugin_filepaths:
-        # Take off `.py` ending if there
-        if plugin_filepath.endswith('.py'):
-            plugin_filepath = plugin_filepath[:-3]
+class PluginLoader(object):
+    def __init__(self):
+        self.loaded_plugins = []
+        self.blacklisted_plugin_filepaths = []
 
-        if '__init__' in os.path:
-            plugin_filepath = os.path.dirname(plugin_filepath)
+    def blacklist_plugin(self, filepath):
+        self.blacklisted_plugin_filepaths.append(filepath)
 
-        plugin_module_name = _get_unique_module_name(plugin_info)
+    def load_plugin(self, plugin_locations, plugin_infos=None):
+        """
+        returns a list of loaded plugins
+        """
+        loaded_plugins = []
+        for plugin_filepath in plugin_filepaths:
+            if plugin_filepath in self.blacklisted_plugin_filepaths:
+                break
 
-        try:
-            if os.path.isdir(plugin_filepath):
+            # Take off `.py` ending if there
+            if plugin_filepath.endswith('.py'):
+                plugin_filepath = plugin_filepath[:-3]
+            
+            # NOTE: think actually want to ADD __init__.py as plugin filepath
+            if '__init__' in os.path.basename(plugin_filepath)
+                plugin_filepath = os.path.dirname(plugin_filepath)
+
+            plugin_module_name = _get_unique_module_name(plugin_info)
+
+            try:
+                if os.path.isdir(plugin_filepath):
+                    # TODO: source file loader cannot take in a directory
+                    pass
+                module = SourceFileLoader(plugin_module_name, plugin_filepath)
+                for attribute in dir(module):
+                    # FIXME
+                    if inspect.isclass(attribute):
+                        loaded_plugins.append(attribute())
+
+
+
+
+    return loaded_plugins
