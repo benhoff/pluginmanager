f7a145cd89a74f3fdc553fc5aa35f4c27b340c76
Ben Hoff <hoff.benjamin.k@gmail.com>
2015-09-25 07:07:34 -0500

    made plugin validators a list

--- simpleyapsy/plugin_loader.py
+++ simpleyapsy/plugin_loader.py
@@ -5,22 +5,34 @@ from importlib.machinery import SourceFileLoader
 from simpleyapsy import plugin_validators
 
 def _get_unique_module_name(plugin_info):
-    plugin_module_name = 'yapsy_loaded_plugin_{name}'.format(plugin_info['name'])
+    plugin_module_name = 'yapsy_plugin_{name}'.format(plugin_info['name'])
     plugin_module_name += '_{number}'
-    for number in range(len(sys.modules)):
+    number = 0
+    while True:
         plugin_module_name.format(number)
         if not plugin_module_name in sys.modules:
             break
+        number += 1
 
     return plugin_module_name
 
 class PluginLoader(object):
     def __init__(self,
-                 module_parser=plugin_validators.IsSubclass()):
+                 module_parser=[plugin_validators.IsSubclass()]):
         self.loaded_plugins = []
         self.processed_filepaths = []
         self.blacklisted_filepaths = []
         self.module_parser = module_parser
+
+    def add_module_parser(self, plugin_validator):
+        if not isinstance(plugin_validator, list):
+            plugin_validator = list(plugin_validator)
+        self.module_parser.extend(plugin_validator)
+
+    def set_module_parser(self, plugin_validator):
+        if not isinstance(plugin_validator, list):
+            plugin_validator = list(plugin_validator)
+        self.module_parser = plugin_validator
 
     def blacklist_plugin(self, filepath):
         self.blacklisted_filepaths.append(filepath)
@@ -34,29 +46,29 @@ class PluginLoader(object):
         """
         for plugin_filepath in plugin_filepaths:
             if plugin_filepath in self.blacklisted_filepaths or plugin_filepath in self.processed_filepaths:
-                break
+                continue
 
-            # Take off `.py` ending if there
-            if plugin_filepath.endswith('.py'):
-                plugin_filepath = plugin_filepath[:-3]
-            
-            # NOTE: think actually want to ADD __init__.py as plugin filepath
             if '__init__' in os.path.basename(plugin_filepath)
                 plugin_filepath = os.path.dirname(plugin_filepath)
 
+            # NOTE: not sure if req'd or not
             plugin_module_name = _get_unique_module_name(plugin_info)
 
             try:
-                if os.path.isdir(plugin_filepath):
-                    # TODO: source file loader cannot take in a directory
-                    pass
-
                 module = SourceFileLoader(plugin_module_name, plugin_filepath)
+                if module.is_package():
+                    # TODO: Implement
+                    pass
+                
+                # NOTE: looks like `load_module` is deprecated `exec_module`
+                module = module.exec_module()
                 self.processed_filepaths.append(plugin_filepath)
+
                 for attribute in dir(module):
                     attribute = getattr(module, attribute)
-                    if self.module_parser.is_valid(attribute):
-                        self.loaded_plugins.append(attribute)
+                    for parser in self.module_parser:
+                        if parser.is_valid(attribute):
+                            self.loaded_plugins.append(attribute)
 
             except Exception:
                 pass
