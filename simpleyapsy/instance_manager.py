17f1a172df5069b0c2bc51a9cf3f03fc3b2dce1e
Ben Hoff <hoff.benjamin.k@gmail.com>
2015-10-08 08:23:50 -0500

    Nominal configuration handeling methods added

--- simpleyapsy/instance_manager.py
+++ simpleyapsy/instance_manager.py
@@ -22,10 +22,21 @@ class InstanceManager(object):
             instance.deactivate()
 
     def get_configuration_templates(self):
-        pass
+        config = {}
+        for instance in self.instances:
+            # TODO: think about name clashing?
+            config[instance.name] = instance.get_configuration_template()
+        return config
 
     def configure_instances(self, config):
-        pass
+        for instance in self.instances:
+            instance.configure(config[instance.name])
 
-    def check_configurations(self):
-        pass
+    def check_configurations(self, config):
+        results = []
+        for instance in self.instances:
+            name = instance.name
+            config_instance = config[name]
+            result = instance.check_configuration(config_instance)
+            results.append((name, result, config_instance))
+        return results
