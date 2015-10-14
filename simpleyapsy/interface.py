cbb402b623edae7563d04bfdd9126617e4bcaaa9
Ben Hoff <hoff.benjamin.k@gmail.com>
2015-10-14 08:58:29 -0500

    Update interface.py

--- simpleyapsy/interface.py
+++ simpleyapsy/interface.py
@@ -27,11 +27,14 @@ class Interface(object):
     def set_plugin_directories(self, paths):
         self.directory_manager.set_directories(paths)
 
+    def remove_plugin_directories(self, paths):
+        self.directory_manager.remove_directories(paths)
+
     def get_plugin_directories(self):
         return self.directory_manager.get_directories()
 
     def track_site_package_paths(self):
-        self.directory_manager.add_site_packages_paths()
+        return self.directory_manager.add_site_packages_paths()
 
     def set_file_getters(self, file_getters):
         self.file_locator.set_file_getters(file_getters)
@@ -39,31 +42,46 @@ class Interface(object):
     def add_file_getters(self, file_getters):
         self.file_locator.add_file_getters(file_getters)
 
+    def remove_file_getters(self, file_getters):
+        self.file_locator.remove_file_getters(file_getters)
+
     def get_file_getters(self):
         return self.file_locator.file_getters
 
-    def locate_plugin_filepaths(self, directories=None):
+    def collect_plugin_filepaths(self, directories=None):
         if directories is None:
             directories = self.get_plugin_directories()
-        return self.file_locator.locate_filepaths(directories)
+        return self.file_locator.collect_filepaths(directories)
 
     def get_plugin_filepaths(self):
         return self.file_locator.get_plugin_filepaths()
 
-    def blacklist_filepaths(self, filepaths):
-        self.module_loader.blacklist_filepaths(filepaths)
+    def add_plugin_filepaths(self, filepaths):
+        self.file_locator.add_plugin_filepaths(filepaths)
+
+    def set_plugin_filepaths(self, filepaths):
+        self.file_locator.set_plugin_filepaths(filepaths)
+
+    def remove_plugin_filepaths(self, filepaths):
+        self.file_locator.remove_plugin_filepaths(filepaths)
+
+    def add_blacklisted_filepaths(self, filepaths):
+        self.module_loader.add_blacklisted_filepaths(filepaths)
 
     def set_blacklisted_filepaths(self, filepaths):
         self.module_loader.set_blacklisted_filepaths(filepaths)
 
+    def remove_blacklisted_filepaths(self, filepaths):
+        self.module_loader.remove_blacklisted_filepaths(filepaths)
+
     def get_blacklisted_filepaths(self):
         return self.module_loader.blacklisted_filepaths
 
-    def load_modules(self, filepaths=None):
+    def collect_modules(self, filepaths=None):
         if filepaths is None:
-            filepaths = self.locate_plugin_filepaths()
+            filepaths = self.collect_plugin_filepaths()
 
-        return self.module_loader.load_modules(filepaths)
+        return self.module_loader.collect_modules(filepaths)
 
     def reload_modules(self, module_or_module_name):
         self.module_loader.reload_module(module_or_module_name)
@@ -71,9 +89,12 @@ class Interface(object):
     def get_loaded_modules(self):
         return self.module_loader.get_loaded_modules()
 
-    def get_plugins_from_modules(self, modules=None):
+    def add_to_loaded_modules(self, modules):
+        self.module_loader.add_to_loaded_modules(modules)
+
+    def collect_plugins_from_modules(self, modules=None):
         if modules is None:
-            modules = self.load_modules()
+            modules = self.collect_modules()
         return self.module_loader.get_plugins_from_modules(modules)
 
     def collect_plugins(self):
@@ -88,6 +109,9 @@ class Interface(object):
 
     def add_plugins(self, plugins):
         self.plugin_manager.add_plugins(plugins)
+
+    def remove_plugins(self, plugins):
+        self.plugin_manager.remove_plugins(plugins)
 
     def get_plugins(self):
         return self.plugin_manager.get_plugins()
