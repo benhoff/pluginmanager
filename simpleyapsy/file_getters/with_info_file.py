6dddedc51c9bd8805499f49652d8fa73226d827b
Ben Hoff <hoff.benjamin.k@gmail.com>
2015-10-15 17:45:13 -0500

    fixed issue with config parser

--- simpleyapsy/file_getters/with_info_file.py
+++ simpleyapsy/file_getters/with_info_file.py
@@ -1,8 +1,13 @@
-import builtins
-# Work around for python 3.2
-FILE_ERROR = getattr(builtins, "FileNotFoundError", "OSError")
+try:
+    import builtins
+    # Work around for python 3.2
+    FILE_ERROR = getattr(builtins, "FileNotFoundError", "OSError")
+    from configparser import ConfigParser
+except:
+    FILE_ERROR = IOError
+    from ConfigParser import ConfigParser
+
 import os
-from configparser import ConfigParser
 from simpleyapsy import util
 from . import PLUGIN_FORBIDDEN_NAME
 
