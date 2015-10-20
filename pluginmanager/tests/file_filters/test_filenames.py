b771fe15624d7b8a746e36f9872994c23e542275
Ben Hoff <hoff.benjamin.k@gmail.com>
2015-10-19 20:07:46 -0500

    added tests for filename

--- pluginmanager/tests/file_filters/test_filenames.py
+++ pluginmanager/tests/file_filters/test_filenames.py
@@ -13,3 +13,9 @@ class TestFilenameFileGetter(unittest.TestCase):
         bad_filename = os.path.join('test/dir/bad.py')
         self.assertTrue(self.file_filter.plugin_valid(filename))
         self.assertFalse(self.file_filter.plugin_valid(bad_filename))
+
+    def test_call(self):
+        filepaths = ['valid/path/__init__.py', 'unvalid/dir/blue.py']
+        filtered = self.file_filter(filepaths)
+        self.assertIn(filepaths[0], filtered)
+        self.assertNotIn(filepaths[1], filtered)
