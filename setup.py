ceb8542b853f4ac2b23c88eeebd6528c10bcb75d
Ben Hoff <hoff.benjamin.k@gmail.com>
2015-10-28 09:16:15 -0500

    release 0.0.6

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(directory, 'README.rst')) as f:
     long_description = f.read()
 setup(
     name="pluginmanager",
-    version='0.0.5',
+    version='0.0.6',
     description='Python Plugin Management, simplified',
     long_description=long_description,
     url='https://github.com/benhoff/pluginmanager',
