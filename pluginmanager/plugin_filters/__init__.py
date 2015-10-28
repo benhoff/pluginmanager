128685fe45daad92dad694a4c45dbce29a634fd1
Ben Hoff <hoff.benjamin.k@gmail.com>
2015-10-28 09:03:15 -0500

    removing depreciated filter

--- pluginmanager/plugin_filters/__init__.py
+++ pluginmanager/plugin_filters/__init__.py
@@ -1,7 +1,6 @@
 from .by_class import by_class
 from .by_name import by_name
-from .by_version import by_version
 from .activated import activated
 from .deactivated import deactivated
 
-__all__ = ['by_class', 'by_name', 'by_version', 'activated', 'deactivated']
+__all__ = ['by_class', 'by_name', 'activated', 'deactivated']
