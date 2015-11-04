cbc96ea9489160e62047c8028187a317500dc280
Ben Hoff <hoff.benjamin.k@gmail.com>
2015-11-03 21:46:41 -0600

    pep8

--- pluginmanager/tests/test_filter_integration.py
+++ pluginmanager/tests/test_filter_integration.py
@@ -16,18 +16,21 @@ class TestIntegration(unittest.TestCase):
         blue_file = str()
         contains_init = []
         contains_blue = []
-        with tempfile.TemporaryDirectory() as temp_dir:
-            self.filter_interface.set_file_filters(pluginmanager.file_filters.FilenameFileFilter())
-            init= os.path.join(temp_dir, '__init__.py')
-            bogus_file = os.path.join(temp_dir, 'bogus.py')
-            blue_file = os.path.join(temp_dir, 'blue.py')
-            open(init, 'a+').close()
-            open(bogus_file, 'a+').close()
-            open(blue_file, 'a+').close()
-            contains_init = self.interface.collect_plugin_filepaths(temp_dir)
-            regex = pluginmanager.file_filters.MatchingRegexFileFilter('blue.py')
-            self.filter_interface.set_file_filters(regex)
-            contains_blue = self.interface.collect_plugin_filepaths(temp_dir)
+        temp_dir = tempfile.TemporaryDirectory()
+        dir_name = temp_dir.name
+        filename_filter = pluginmanager.file_filters.FilenameFileFilter()
+        self.filter_interface.set_file_filters(filename_filter)
+        init = os.path.join(dir_name, '__init__.py')
+        bogus_file = os.path.join(dir_name, 'bogus.py')
+        blue_file = os.path.join(dir_name, 'blue.py')
+        open(init, 'a+').close()
+        open(bogus_file, 'a+').close()
+        open(blue_file, 'a+').close()
+        contains_init = self.interface.collect_plugin_filepaths(dir_name)
+        regex = pluginmanager.file_filters.MatchingRegexFileFilter('blue.py')
+        self.filter_interface.set_file_filters(regex)
+        contains_blue = self.interface.collect_plugin_filepaths(dir_name)
+        temp_dir.cleanup()
         self.assertIn(init, contains_init)
         self.assertIn(blue_file, contains_blue)
         self.assertNotIn(bogus_file, contains_init)
@@ -39,8 +42,8 @@ class TestIntegration(unittest.TestCase):
         module.plugin = plugin
         bogus = 'five'
         module.bogus = bogus
-        self.filter_interface.set_module_filters(pluginmanager.module_filters.SubclassParser())
-        cotains_plugin = self.interface.collect_plugins(module)
+        subclass_parser = pluginmanager.module_filters.SubclassParser()
+        self.filter_interface.set_module_filters(subclass_parser)
+        contains_plugin = self.interface.collect_plugins(module)
         self.assertIn(plugin, contains_plugin)
-        self.assertNotIn(bougs, contains_plugin)
-
+        self.assertNotIn(bogus, contains_plugin)
