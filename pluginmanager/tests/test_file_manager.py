98bd8df75008622fda0da6d719fdfd4e6047d356
Ben Hoff <hoff.benjamin.k@gmail.com>
2015-11-03 21:31:44 -0600

    added test for failing blacklist in file manager

--- pluginmanager/tests/test_file_manager.py
+++ pluginmanager/tests/test_file_manager.py
@@ -54,5 +54,20 @@ class TestFileManager(unittest.TestCase):
         self.assertIn('test/dir', filtered_filepaths)
         self.assertNotIn('dir/test', filtered_filepaths)
 
+    def test_blacklist_filepaths(self):
+        self.file_manager.set_file_filters([])
+        temp_dir = tempfile.TemporaryDirectory()
+        file_template = os.path.join(temp_dir.name, '{}.py')
+        norm_file = file_template.format('norm')
+        blacklist_file = file_template.format('blacklist')
+        open(norm_file, 'a+').close()
+        open(blacklist_file, 'a+').close()
+        self.file_manager.add_blacklisted_filepaths(blacklist_file)
+        filepaths = self.file_manager.collect_filepaths(temp_dir.name)
+        temp_dir.cleanup()
+        self.assertNotIn(blacklist_file, filepaths)
+        self.assertIn(norm_file, filepaths)
+
+
 if __name__ == '__main__':
     unittest.main()
