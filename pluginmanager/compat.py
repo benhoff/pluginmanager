bf265d5024ea7578f3cfde141a8301897a81392d
Ben Hoff <hoff.benjamin.k@gmail.com>
2015-11-05 09:23:15 -0600

    fixed metaclass issue with python2

--- pluginmanager/compat.py
+++ pluginmanager/compat.py
@@ -7,6 +7,30 @@ except ImportError:
     # https://github.com/pypa/virtualenv/issues/355
     from distutils.sysconfig import get_python_lib as getsitepackages
 
+def with_metaclass(meta, *bases):
+    """
+    function from jinja2/_compat.py. License: BSD.
+
+    Use it like this::
+
+        class BaseForm(object):
+            pass
+
+        class FormType(type):
+            pass
+
+        class Form(with_metaclass(FormType, BaseForm)):
+            pass
+    """
+    class metaclass(meta):
+        __call__ = type.__call__
+        __init__ = type.__init__
+        def __new__(cls, name, this_bases, d):
+            if this_bases is None:
+                return type.__new__(cls, name, (), d)
+            return meta(name, bases, d)
+    return metaclass('temporary_class', None, {})
+
 _ver = sys.version_info
 
 is_py2 = (_ver[0] == 2)
@@ -15,6 +39,7 @@ is_py3 = (_ver[0] == 3)
 if is_py2:
     import imp
     load_source = imp.load_source
+    reload = imp.reload
 if is_py3:
     if _ver[1] >= 4:
         # flake8: noqa
