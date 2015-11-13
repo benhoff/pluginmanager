a2587d3ea735de281e9d5795c4378463b0cde186
Ben Hoff <hoff.benjamin.k@gmail.com>
2015-11-12 19:51:04 -0600

    checking for attr in pluginmanager

--- pluginmanager/plugin_filters/activated.py
+++ pluginmanager/plugin_filters/activated.py
@@ -1,7 +1,7 @@
 def activated(plugins):
     activated = []
     for plugin in plugins:
-        if plugin.active:
+        if hasattr(plugin, 'active') and plugin.active:
             activated.append(plugin)
 
     return activated
--- pluginmanager/plugin_filters/deactivated.py
+++ pluginmanager/plugin_filters/deactivated.py
@@ -1,7 +1,7 @@
 def deactivated(plugins):
     deactivated = []
     for plugin in plugins:
-        if not plugin.active:
+        if hasattr(plugin, 'active') and not plugin.active:
             deactivated.append(plugin)
 
     return deactivated
