e323d1015abff1b48082db8e6971046b05e56087
Ben Hoff <hoff.benjamin.k@gmail.com>
2016-01-07 06:17:36 -0600

    initial take on plugin manager docs

--- pluginmanager/plugin_manager.py
+++ pluginmanager/plugin_manager.py
@@ -4,38 +4,106 @@ from .iplugin import IPlugin
 
 
 class PluginManager(object):
+    """
+    PluginManager manages the plugin state. It can also optionally be used to
+    automatically instantiate classes. It can also be used to optionally
+    enforce uniqueness in plugins. Both of these options are set to `True` by
+    default. While this slightly violates the principle of least suprise, it's
+    easier to manage in the library context.
+    """
     def __init__(self,
                  unique_instances=True,
-                 instantiate_classes=True):
-
-        self.plugins = []
+                 instantiate_classes=True,
+                 plugins=None,
+                 blacklisted_plugins=None):
+        """
+        unique_instances enforces uniquness on plugins.
+        instatntiate_classes is a helper method to instatnatiate classes.
+        plugins can be a single obj or iterable
+        blacklisted plugins can be a single obj or iterable
+        """
         self.unique_instances = unique_instances
         self.instantiate_classes = instantiate_classes
-        self.blacklisted_plugins = []
+
+        if plugins is None:
+            plugins = []
+        if blacklisted_plugins is None:
+            blacklisted_plugins = []
+
+        self.plugins = util.return_list(plugins)
+        self.blacklisted_plugins = util.return_list(blacklisted_plugins)
 
     def get_plugins(self, filter_function=None):
+        """
+        Gets out the plugins from the internal state.
+        If the optional filter_function is supplied, applies the filter
+        function to the arguments before returning them. Filters should
+        be callable and take a list argument of plugins.
+        """
         plugins = self.plugins
         if filter_function is not None:
             plugins = filter_function(plugins)
         return plugins
 
     def add_plugins(self, plugins):
+        """
+        Adds plugins to the internal state.
+        If `instatntiate_classes` is True and the plugins
+        have class instances in them, attempts to instatiate the classes.
+
+        If `unique_instances` is True and duplicate instances are passed in,
+        this method will not track the new instances internally.
+        """
         self._instance_parser(plugins)
 
     def set_plugins(self, plugins):
+        """
+        sets plugins to the internal state.
+        If `instatntiate_classes` is True and the plugins
+        have class instances in them, attempts to instatiate the classes.
+
+        If `unique_instances` is True and duplicate instances are passed in,
+        this method will not track the new instances internally.
+        """
         self.plugins = []
         self._instance_parser(plugins)
 
     def remove_plugins(self, plugins):
+        """
+        removes plugins from the internal state
+        """
         util.remove_from_list(self.plugins, plugins)
 
     def remove_instance(self, instances):
+        """
+        removes instances from the internal state.
+
+        Note that this method is syntatic sugar for the
+        `remove_plugins` method call and calls that function
+        """
         self.remove_plugins(instances)
 
     def _get_instance(self, klasses):
+        """
+        internal method that gets every instance of the klasses
+        out of the internal plugin state.
+        """
         return [x for x in self.plugins if isinstance(x, klasses)]
 
     def get_instances(self, filter_function=IPlugin):
+        """
+        Gets instances out of the internal state using
+        the default filter supplied in filter_function.
+        By default, it is the class IPlugin.
+
+        Can optionally pass in a list or tuple of classes
+        in for `filter_function` which will accomplish
+        the same goal.
+
+        lastly, a callable can be passed in, however
+        it is up to the user to determine if the
+        objects are instances or not.
+        """
         if isinstance(filter_function, (list, tuple)):
             return self._get_instance(filter_function)
         elif inspect.isclass(filter_function):
@@ -55,6 +123,13 @@ class PluginManager(object):
             IPlugin.register(klass)
 
     def _instance_parser(self, plugins):
+        """
+        internal method to parse instances of plugins.
+
+        Determines if each class is a class instance or
+        object instance and calls the appropiate handler
+        method.
+        """
         plugins = util.return_list(plugins)
         for instance in plugins:
             if inspect.isclass(instance):
@@ -63,6 +138,14 @@ class PluginManager(object):
                 self._handle_object_instance(instance)
 
     def _handle_class_instance(self, klass):
+        """
+        handles class instances. If a class is blacklisted, returns.
+        If uniuqe_instances is True and the class is unique, instantiates
+        the class and adds the new object to plugins.
+
+        If not unique_instances, creates and adds new instance to plugin
+        state
+        """
         if (klass in self.blacklisted_plugins or not
                 self.instantiate_classes or
                 klass == IPlugin):
@@ -86,26 +169,54 @@ class PluginManager(object):
             self.plugins.append(instance)
 
     def activate_plugins(self):
+        """
+        helper method that attempts to activate plugins
+        """
         for instance in self.get_instances():
-            instance.activate()
+            if hasattr(instance, 'activate'):
+                instance.activate()
 
     def deactivate_plugins(self):
+        """
+        helper method that attempts to deactivate plugins.
+        """
         for instance in self.get_instances():
-            instance.deactivate()
+            if hasattr(instance, 'deactivate'):
+                instance.deactivate()
 
     def _unique_class(self, cls):
+        """
+        internal method to check if any of the plugins are instances
+        of a given cls
+        """
         return not any(isinstance(obj, cls) for obj in self.plugins)
 
     def add_blacklisted_plugins(self, plugins):
+        """
+        add blacklisted plugins.
+        `plugins` may be a single object or iterable.
+        """
         plugins = util.return_list(plugins)
         self.blacklisted_plugins.extend(plugins)
 
     def set_blacklisted_plugins(self, plugins):
+        """
+        sets blacklisted plugins.
+        `plugins` may be a single object or iterable.
+        """
         plugins = util.return_list(plugins)
         self.blacklisted_plugins = plugins
 
     def get_blacklisted_plugins(self):
+        """
+        gets blacklisted plugins tracked in the internal state
+        Returns a list object.
+        """
         return self.blacklisted_plugins
 
     def remove_blacklisted_plugins(self, plugins):
+        """
+        removes `plugins` from the blacklisted plugins.
+        `plugins` may be a single object or iterable.
+        """
         util.remove_from_list(self.blacklisted_plugins, plugins)
