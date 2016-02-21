818521f80ac4d334137c7e8af842cd7d9914b12f
Ben Hoff <hoff.benjamin.k@gmail.com>
2016-02-21 09:27:10 -0700

    added in documentation for entry point manager

--- pluginmanager/entry_point_manager.py
+++ pluginmanager/entry_point_manager.py
@@ -3,6 +3,10 @@ from pluginmanager import util
 
 
 class EntryPointManager(object):
+    """
+    Manages the state associated with the entry points and is responsible for
+    collecting points using entry points.
+    """
     def __init__(self, entry_point_names=None):
         if entry_point_names is None:
             entry_point_names = set()
@@ -10,24 +14,56 @@ class EntryPointManager(object):
         self.entry_point_names = util.return_set(entry_point_names)
 
     def add_entry_points(self, names):
+        """
+        adds `names` to the internal collection of entry points to track
+
+        `names` can be a single object or an iterable but
+        must be a string or iterable of strings.
+        """
         names = util.return_set(names)
         self.entry_point_names.update(names)
 
     def set_entry_points(self, names):
+        """
+        sets the internal collection of entry points to be
+        equal to `names`
+
+        `names` can be a single object or an iterable but
+        must be a string or iterable of strings.
+        """
         names = util.return_set(names)
         self.entry_point_names = names
 
     def remove_entry_points(self, names):
+        """
+        removes `names` from the set of entry points to track.
+
+        `names` can be a single object or an iterable.
+        """
         names = util.return_set(names)
         util.remove_from_set(self.entry_point_names,
                              names)
 
     def get_entry_points(self):
+        """
+        returns a set of all the entry points tracked internally
+        """
         return self.entry_point_names
 
     def collect_plugins(self,
                         entry_points=None,
                         verify_requirements=False):
+
+        """
+        collects the plugins from the `entry_points`. If `entry_points` is not explicitly
+        defined, this method will pull from the internal state defined using the add/set entry points
+        methods.
+
+        `entry_points` can be a single object or an iterable, but must be either a string or an
+        iterable of strings.
+
+        returns a list of plugins or an empty list.
+        """
 
         if entry_points is None:
             entry_points = self.entry_point_names
